da114c5b1518ea0f3939f7a62c2e68f3cd3173e1
chriswang1990 <1990chriswang1990@gmail.com>
2016-11-14 23:45:59 -0800

    Create the Treasure class and display them in template with python script

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context = {'treasure_name' : name,
-               'treasure_val' : value}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'treasures': treasures})
 
-# Create your views here.
+
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+             Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+             Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA')]
