0d8a90fd91737ef59c8149d1538642962f03e025
chriswang1990 <1990chriswang1990@gmail.com>
2016-11-14 21:56:42 -0800

    Add a html template with data parsed from view

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('<h1>Hello Explorers!</h1>')
+    name = 'Gold Nugget'
+    value = 1000.00
+    context = {'treasure_name' : name,
+               'treasure_val' : value}
+    return render(request, 'index.html', context)
 
 # Create your views here.
